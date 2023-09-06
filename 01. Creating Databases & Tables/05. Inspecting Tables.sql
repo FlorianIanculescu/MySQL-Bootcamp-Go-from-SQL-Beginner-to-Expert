91f8f2942c4ddc4f0e0d804f57d787df552de2f0
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2023-09-06 10:53:19 +0300

    Add files via upload

---
+++ 01. Creating Databases & Tables/05. Inspecting Tables.sql
@@ -0,0 +1,11 @@
+-- To show tables in database:
+
+SHOW tables;
+
+-- To show columns in a specific table:
+
+SHOW COLUMNS FROM cats;
+
+-- To show columns in a specific table (short version):
+
+DESC cats;
